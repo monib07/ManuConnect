b49cc04510ff6f248705b66ac193f3b4154317b6
imsaiful <saifulcseng@gmail.com>
2017-07-28 06:41:56 +0530

    add conatct detail, google map and call functionality in order to contact the university.

---
+++ app/src/main/java/com/example/monib/manuconnect/ContactActivity.java
@@ -0,0 +1,62 @@
+package com.example.monib.manuconnect;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ContactActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private Button call;
+    String number="04023006612";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+        call=(Button)findViewById(R.id.bcall);
+    }
+    public void Call(View v)
+    {
+        Intent i=new Intent(Intent.ACTION_DIAL);
+
+
+            i.setData(Uri.parse("tel:"+number));
+            Toast.makeText(getApplicationContext(),"Calling",Toast.LENGTH_SHORT).show();
+
+        startActivity(i);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng sydney = new LatLng(17.425055, 78.364364);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("MANUU"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
